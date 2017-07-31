6f10208cfd2eda89ca8374ae5c12bc7903ff4849
Lyndon <snakeliwei@gmail.com>
2017-07-31 17:33:36 +0800

    init

---
+++ src/router/index.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Login from '@/components/Login'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [{
+    path: '/',
+    name: 'Login',
+    component: Login
+  },
+  {
+    path: '*',
+    redirect: '/' // 当不存在页面时 ，重定向到首页
+  }]
+})
